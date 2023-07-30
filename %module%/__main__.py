5860a27ae0f880cd70c39881c9de35fe8f3c85ee
naek <naek2k@outlook.com>
2023-07-30 18:48:15 -0400

    init

---
+++ %module%/__main__.py
@@ -0,0 +1,8 @@
+"""%module% main."""
+
+def main() -> None:
+    """Entry point for %module%."""
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Setup for %module%."""
+
+from distutils.core import setup
+
+setup(
+    name="%module%",
+    version="1.0",
+    description="%module%",
+    author="%author%",
+    packages=["%module%", "%module%.%module%"],
+    entry_points={"console_scripts": ["%module%=%module%.__main__:main"]},
+)
