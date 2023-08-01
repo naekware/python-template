f8b49b41ca7013b4c5b8433048bc8ffefbdecb92
naek <naek2k@outlook.com>
2023-08-01 18:35:07 -0400

    updating template

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(
     name="%module%",
     version="1.0",
     description="%module%",
     author="%author%",
-    packages=["%module%"],
+    packages=find_packages(),
+    package_data={"%module%": ["py.typed"]},
+    install_requires=[],
     entry_points={"console_scripts": ["%module%=%module%.__main__:main"]},
 )
